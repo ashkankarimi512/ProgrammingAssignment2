316b1b3d23e8899ff23b6d8949315cba896d3b19
ashkankarimi512 <akarimi@logictory.com>
2014-12-17 21:11:00 -0800

    unit test for makeCacheMatrix and cacheSolve

---
+++ unitTestCacheMatrix.R
@@ -0,0 +1,22 @@
+##
+## Unit Test for : cacheMatrix.R
+##
+
+unitTest_CacheMatrix <- function() {
+        a <- matrix( c(30, 72, 87, 96, 74, 46, 86, 85, 57), nrow=3, ncol=3)
+        print(a)
+        
+        ca <- makeCacheMatrix( a )
+        ainv <- cacheSolve( ca )
+        print(ainv)
+        
+        print("Matrix A")
+        print(ca$get() )
+        
+        print("A inverse")
+        print(ca$getInvMat() )
+        
+        print("A inverse * A")
+        print(ainv %*% a )
+        ca
+}
