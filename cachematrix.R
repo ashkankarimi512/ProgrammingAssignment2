4ff24f29cafa6bcff985f6464a5eb7206ebfdd4e
ashkankarimi512 <akarimi@logictory.com>
2014-12-17 20:49:37 -0800

    Make correction to comments and clean up indentation

--- cachematrix.R
+++ cachematrix.R
@@ -1,4 +1,7 @@
 ##
+## Programming assignment 2: Lexical Scoping
+##
+##
 ## function: makeCacheMatrix
 ## This function returns a list of function used for caching a matrix
 ## and its inverse. 
@@ -10,6 +13,14 @@
 ##  ainv <- cacheSolve( ca )
 ##  ca$get()
 ##  ca$getInvMat()
+##
+## Test that the inverse is correct:
+##       ainv %*% a        must equal the Identity matrix.
+##
+## Notes:  Operator <<- used to assign values to a variable with in the
+## specific environment.  Here, the function caches the supplied matrix
+## the inverse within special caced matrix returned.  The list returned 
+## contains for method to get/set the matrix and its inverse.
 ## 
 
 makeCacheMatrix <- function(xmat = matrix()) {
@@ -25,28 +36,41 @@ makeCacheMatrix <- function(xmat = matrix()) {
         
         getInvMat <- function() matInv
         
-        list(set = set, get = get, setInvMat = setInvMat, getInvMat = getInvMat)
+        list(set = set, get = get, setInvMat = setInvMat, 
+             getInvMat = getInvMat)
 }
 
 
 ## function: cacheSolve
 ##
-## This function calculates the invese of the xmat or returns the cached Inverse
-## it it has not been changed.
-#
+## This function calculates the inverse of the xmat and caches the
+## matrix and the inverse of the matrix. The inverse is computed using
+## solve function from the R base package.
+##
+## The function returns the cached Inverse matrix.
+## 
+##
 cacheSolve <- function(xmat, ...) {
         ## Return a matrix that is the inverse of 'xmat'
+        
+        ## get the cached inverse matrix
         matInv <- xmat$getInvMat()
+        
+        ## Test if the cached inverse matrix is not null return it.
         if ( !is.null(matInv)) {
                 message("getting cached data")
                 return(matInv)
         }
         
+        ## get the cached matrix 
         data <- xmat$get()
-        ## Assume the martrix has an inverse.
-        ## TBD : check if the matrix has an inverst!
+        
+        ## Calculate the inverse and save it in the
+        ## cache.  Assume the martrix has an inverse.
+        ## To be done for future enhancement : 
+        ## check if the matrix has an inverse!
         
         matInv <- solve(data)
-        xmat$setInvMat( matInv )
+        xmat$setInvMat( matInv )  ## Save the inverse to the cache
         matInv
 }
