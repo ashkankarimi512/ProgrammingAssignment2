f0ecd788f959c6f0e599d2984c63568bf470ebfc
ashkankarimi512 <akarimi@logictory.com>
2014-12-17 17:36:57 -0800

    initial check in for assignment2

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,41 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(xmat = matrix()) {
+        matInv <- NULL
+        set <- function(y) {
+                xmat <<- y
+                matInv <<- NULL
+        }
+        
+        get <- function() xmat
+        
+        setInvMat <- function(y) matInv <<- y
+        
+        getInvMat <- function() matInv
+        
+        list(set = set, get = get, setInvMat = setInvMat, getInvMat = getInvMat)
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## function: cacheSolve
+##
+## This function calculates the invese of the xmat or returns the cached Inverse
+## it it has not been changed.
+#
+cacheSolve <- function(xmat, ...) {
+        ## Return a matrix that is the inverse of 'xmat'
+        matInv <- xmat$getInvMat()
+        if ( !is.null(matInv)) {
+                message("getting cached data")
+                return(matInv)
+        }
+        
+        data <- xmat$get()
+        ## Assume the martrix has an inverse.
+        ## TBD : check if the matrix has an inverst!
+        
+        matInv <- solve(data)
+        xmat$setInvMat( matInv )
+        matInv
 }
